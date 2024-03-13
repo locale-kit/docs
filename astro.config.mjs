ff1c8e7a3f060d0b2b8e71a1aec994ef92c1808c
Jake Lees @brocococonut <brocococonut@gmail.com>
2024-03-13 23:55:16 +1100

    Add GitHub Pages deployment workflow and update Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,29 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  site: "https://locale-kit.github.io",
+  base: "/docs",
+  integrations: [
+    starlight({
+      title: "LocaleKit",
+      social: {
+        github: "https://github.com/locale-kit",
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", link: "/guides/example/" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+    }),
+  ],
 });
